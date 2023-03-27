3451e7a6ba72dcd8be2f407520830795a7b64120
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2023-03-27 02:24:36 -0400

    Update project1.s
    Design the main function

--- project1.s
+++ project1.s
@@ -1,3 +1,10 @@
 .data
-input:  .space 11     # to store user input (10 characters + null terminator)
-newline: .asciiz "\n" # for printing a newline character
+input:  .space 11	# to store user input (10 characters + null terminator)
+newline: .asciiz "\n"	# for printing a newline character
+
+.text
+.globl main
+main:
+   	# calculate N and M
+	# prompt user for input
+	# process input
